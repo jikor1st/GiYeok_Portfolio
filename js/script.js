2a28312d37f4dc0d0f046ddf4fe2ef71317f127a
jikor1st <54614268+jikor1st@users.noreply.github.com>
2020-09-28 04:17:20 +0900

    change

--- js/script.js
+++ js/script.js
@@ -3,10 +3,29 @@ console.log("javascript load");
 const hamBtn = document.querySelector("nav > .hamBtn");
 let navHamLi = document.querySelectorAll("nav a.hamBtn > div.hamLine");
 navHamLi = Array.prototype.slice.call(navHamLi);
+const navHam = document.querySelector('ul.navHam');
+var navHamEls = document.querySelectorAll('ul.navHam > li');
+navHamEls = Array.prototype.slice.call(navHamEls);
+
 var hamToggle = true;
 
+const minusBtn = document.querySelector('.stBtn > .minus');
+const plusBtn = document.querySelector('.stBtn > .plus');
+const mainG = document.querySelector('.giyeok_svg > .mainG');
+const fontWeightP = document.querySelector('p.fontWeight');
+
+reset();
+
 function reset(){
-    minusBtn.style.visibility = "hidden";
+    minusBtn.style.transform = "scale(0)";
+    let hamD = 0;
+    for(var i = 0;i < navHamEls.length;i++){
+        navHamEls[i].style.transition = "all 0.8s cubic-bezier(0.68, -0.6, 0.32, 1.6) " + hamD + "s";
+        navHamEls[i].style.visibility = "hidden";
+        navHamEls[i].style.transform = "rotateZ(25deg) translateY(-20px)";
+        navHamEls[i].style.opacity = "0";
+        hamD += 0.2;
+    }
 }
 
 function onClickHamBtn(e){
@@ -17,11 +36,25 @@ function onClickHamBtn(e){
             navHamLi[1].style.opacity = 0;
             navHamLi[2].style.transform = "translateY(2px) rotateZ(-45deg)";
             navHamLi[2].style.width = "100%";
+
+            for(var i = 0;i < navHamEls.length;i++){
+                navHamEls[i].style.visibility = "visible";
+                navHamEls[i].style.transform = "rotateZ(0) translateY(0)";
+                navHamEls[i].style.opacity = "1";
+            }
+            
         }else{
             navHamLi[0].style.transform = "";
             navHamLi[1].style.opacity = 1;
             navHamLi[2].style.transform = "";
             navHamLi[2].style.width = "60%";
+
+            for(var i = 0;i < navHamEls.length;i++){
+                navHamEls[i].style.transform = "rotateZ(25deg) translateY(-20px)";
+                navHamEls[i].style.opacity = "0";
+                navHamEls[i].style.visibility = "hidden";
+            }
+            
         }
         hamToggle = !hamToggle;
     }
@@ -29,10 +62,7 @@ function onClickHamBtn(e){
 
 hamBtn.addEventListener("click", onClickHamBtn);
 
-const minusBtn = document.querySelector('.stBtn > .minus');
-const plusBtn = document.querySelector('.stBtn > .plus');
-const mainG = document.querySelector('.giyeok_svg > .mainG');
-const fontWeightP = document.querySelector('p.fontWeight');
+
 
 let mainS = 170;
 let sL = 30;
